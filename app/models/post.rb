5ce187064bfc91ea00d9920b6dfa614bc7248198
Yisrael Gurkow <yisrelg3@gmail.com>
2021-07-01 12:42:06 -0400

    commit tree fc4fcf50428d9fdec96a9ca4cfd394fd70bfeb89

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :good_clickbait
+
+    def good_clickbait
+        good_titles = ["Won't Believe","Secret","Top 7","Guess"]
+        if title && good_titles.none?{|string| title.include?(string)}
+            errors.add(:title, "must include one of the folowing " + '"' + good_titles.join('", "')+'"')
+            puts self.errors.full_messages
+        end
+    end
 end
